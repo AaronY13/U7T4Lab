8ef40b5e510c579db9b4b537ad3f8c77c4bfa44a
BT_1E10_21 <sample@bths.edu>
2022-02-11 09:42:16 -0500

    Initial commit

---
+++ src/ArrayListAlgorithms.java
@@ -0,0 +1,325 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class ArrayListAlgorithms {
+    /**
+     * Returns true if any of the elements in stringList contain
+     * target as a substring; false otherwise.  Use indexOf instead of contains
+     * for AP practice! (although in reality, contains is preferred)
+     * <p>
+     * Does NOT mutate (modify) elements of stringList.
+     * PRECONDITION: stringList.size() > 0
+     *
+     * @param stringList original arraylist of Strings
+     * @return true if target is found in any of the strings, false otherwise
+     */
+    public static boolean containsTarget(ArrayList<String> stringList, String target)
+    {
+        for (int i = 0; i < stringList.size(); i ++)
+        {
+            for (int n = 0; n < stringList.get(i).length(); n++)
+            {
+                if (stringList.get(i).indexOf(target) != -1)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    /** Returns number of elements in intList that are less than the
+     *  average of all elements.
+     *
+     *  Does NOT mutate (modify) elements of intList.
+     *  PRECONDITION: intList.size() > 0
+     *
+     *  @param intList  original arraylist of Integers
+     *  @return  the number of ints in intList that are less than the average
+     */
+    public static int belowAverage(ArrayList<Integer> intList)
+    {
+        int sum = 0;
+        double avg = 0;
+        int count = 0;
+
+        for (int i = 0; i < intList.size(); i++)
+        {
+            sum += intList.get(i);
+        }
+        avg = (double) sum / intList.size();
+
+        for (int n = 0; n < intList.size(); n++)
+        {
+            if (intList.get(n) < avg)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    /** Replaces all words in wordList that end in "s" with the all-uppercase
+     *  version of the word.  For example, "apples" should be replaced with "APPLES".
+     *  Assume all letters of all words in wordList are lowercase initially (no need
+     *  to worry about checking for case or converting first to lowercase)
+     *
+     *  DOES mutate (modify) elements of wordList.
+     *  PRECONDITION: wordList.size() > 0
+     *
+     *  @param wordList  original arraylist of words
+     */
+    public static void replaceWithCaps(ArrayList<String> wordList)
+    {
+        for (int i = 0; i < wordList.size(); i++)
+        {
+            if (wordList.get(i).substring(wordList.get(i).length() - 1).equals("s"))
+            {
+                wordList.set(i, wordList.get(i).toUpperCase());
+            }
+        }
+    }
+
+    /** Returns the index at which the minimum value of all integers in
+     *  intList appears; if the minimum value appears more than once in
+     *  the arraylist, return the index of the first occurrence
+     *
+     *  Does NOT mutate (modify) elements in intList.
+     *  PRECONDITION: intList.size() > 0
+     *
+     *  @param intList  original arraylist of Integers
+     *  @return  the index at which the minimum value occurs
+     */
+    public static int indexOfMinimum(ArrayList<Integer> intList)
+    {
+        int min = intList.get(0);
+        int index = 0;
+        for (int i = 0; i < intList.size(); i++)
+        {
+            if (intList.get(i) < min)
+            {
+                min = intList.get(i);
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    /** Returns true if two array lists of the same length contain the exact
+     *  same elements in the same order (i.e. the two arraylists are identical).
+     *  Returns false otherwise.
+     *
+     *  Does NOT mutate (modify) elements in either arraylist
+     *  PRECONDITION: numList1.size() == numList2.size()
+     *
+     *  @param numList1  first arraylist of Integers
+     *  @param numList2  second arraylist of Integers, has the same size a first
+     *  @return  true if both arraylists are identical, element for element
+     */
+    public static boolean areIdentical(ArrayList<Integer> numList1, ArrayList<Integer> numList2)
+    {
+        for (int i = 0; i < numList1.size(); i++)
+        {
+            if (numList1.get(i) != numList2.get(i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /** Removes all elements from numList that are ODD Integers.
+     *
+     *  DOES mutate (modify) elements in numList
+     *  PRECONDITION: numList1.size() > 0
+     *
+     *  @param numList  arraylist of Integers
+     */
+    public static void removeOdds(ArrayList<Integer> numList)
+    {
+        for (int i = 0; i < numList.size(); i++)
+        {
+            if (numList.get(i) % 2 == 1)
+            {
+                numList.remove(i);
+                i--;
+            }
+        }
+    }
+
+    /** Removes all elements from wordList that contain an a, e, i , and/or o.
+     *  All other words (i.e. those that have u and/or y as the vowel
+     *  such as "ugh" or "sly", or no vowels, like "psh"), add a duplicate of
+     *  that element to wordList at an adjacent index.
+     *
+     *  Assume all words have lowercase letters (i.e. no need to check for case)
+     *
+     *  DOES mutate (modify) elements in wordList
+     *  PRECONDITION: wordList.size() > 0
+     *
+     *  @param wordList  arraylist of Strings
+     */
+    public static void wackyVowels(ArrayList<String> wordList)
+    {
+        for (int i = 0; i < wordList.size(); i++)
+        {
+            if (wordList.get(i).contains("a") || wordList.get(i).contains("e") || wordList.get(i).contains("i") ||wordList.get(i).contains("o"))
+            {
+                 wordList.remove(i);
+                 i--;
+            }
+            else
+            {
+                wordList.add(i, wordList.get(i));
+                i++;
+            }
+        }
+    }
+
+    /** Removes all duplicate values from an intList, leaving only the first
+     *  occurrence in the arrayList; for example, this method will modify
+     *  [1, 2, 5, 4, 2, 2, 1, 6, 4, 4, 8, 1, 7, 4, 2] --> [1, 2, 5, 4, 6, 8, 7]
+     *
+     *  DOES mutate (modify) elements in intList
+     *  PRECONDITION: intList.size() > 0
+     *
+     *  @param intList  intList of Integers
+     */
+    public static void removeDuplicates(ArrayList<Integer> intList)
+    {
+        ArrayList<Integer> duplicates = new ArrayList<>();
+        for (int i = 0; i < intList.size(); i++)
+        {
+            if (duplicates.contains(intList.get(i)))
+            {
+                intList.remove(i);
+                i--;
+            }
+            else
+            {
+                duplicates.add(intList.get(i));
+            }
+        }
+    }
+
+    /** Adds an uppercase version of each string directly AFTER the string
+     *  in wordList; for example, if wordList is ["hello", "my", "best", "friend"]
+     *  this method modifies wordList to be:
+     *  ["hello", "HELLO", "my", "MY", "best", "BEST", "friend", "FRIEND"]
+     *
+     *  Assume all words have lowercase letters originally
+     *
+     *  DOES mutate (modify) elements in wordList
+     *  PRECONDITION: wordList.size() > 0
+     *
+     *  @param wordList  arraylist of Strings
+     */
+    public static void duplicateUpperAfter(ArrayList<String> wordList)
+    {
+        for (int i = 0; i < wordList.size(); i++)
+        {
+            wordList.add(i + 1, wordList.get(i).toUpperCase());
+            i++;
+        }
+    }
+
+    /** Removes all words from wordList that begin with "b" and inserts them at the
+     *  front of wordList; all "b" words that are moved should appear in the same order
+     *  in the modified arrayList as they did before being moved
+     *
+     *  For example, this method will take a wordList:
+     *  ["apple", "banana", "cherry", "donut", "bagel", "danish", "berry", "baguette", "soda"]
+     *  and modify it to
+     *  ["banana", "bagel", "berry", "baguette", "apple", "cherry", "donut", "danish", "soda"]
+     *
+     *  DOES mutate (modify) elements in wordList
+     *  PRECONDITIONS: wordList.size() > 0, all strings in wordList have at least one character
+     *
+     *  @param wordList  arraylist of words
+     */
+    public static void moveBWords(ArrayList<String> wordList)
+    {
+        int index = 0;
+        for (int i = 0; i < wordList.size(); i++)
+        {
+            if (wordList.get(i).indexOf("b") == 0)
+            {
+                String word = wordList.remove(i);
+                wordList.add(index, word);
+                index++;
+            }
+        }
+    }
+
+    /** Returns arraylist of Integers that contain all mode(s) of the int array numList.
+     *  If elements in numList all appear exactly once, there is no mode, and this method
+     *  should return an empty list
+     *
+     *  For example, if numList is: [1, 2, 3, 2, 4, 5, 5, 6],
+     *  then numList contains two modes: 2, 5
+     *  and this method returns an arraylist containing 2 and 5 (in any order)
+     *  If numList is: [1, 2, 3, 2, 4, 5, 5, 6, 6, 7, 6],
+     *  then numList contains one mode: 6
+     *  and this method returns an arrayList containing 6
+     *  If numList is: [1, 2, 3, 4, 5, 6], then numList contains no mode
+     *  and this method returns an empty arrayList: []
+     *  If numList is: [2, 2, 2, 3, 3, 3, 4, 4, 4],
+     *  then numList contains three modes: 2, 3, 4
+     *  and this method returns an arrayList containing 2, 3, and 4 (in any order)
+     *
+     *  Does NOT mutate (modify) elements in numList
+     *  PRECONDITIONS: numList.length > 0
+     *
+     *  @param numList  numList of ints
+     */
+    public static ArrayList<Integer> modes(int[] numList)
+    {
+
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        for (int num : numList)
+        {
+            nums.add(num);
+        }
+        removeDuplicates(nums);
+
+        ArrayList<Integer> count = new ArrayList<>();
+        int mode = 1;
+        int max = 1;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int n = 0; n < numList.length; n++)
+            {
+
+                if (nums.get(i) == numList[n])
+                {
+                    mode++;
+                }
+            }
+            count.add(mode);
+            mode = 1;
+        }
+        for (int j = 0; j < count.size(); j++)
+        {
+            if (count.get(j) > max)
+            {
+                max = count.get(j);
+            }
+        }
+        ArrayList<Integer> modes = new ArrayList<>();
+        for (int m = 0; m < count.size(); m++)
+        {
+            if (count.get(m) == max)
+            {
+                modes.add(nums.get(m));
+            }
+        }
+
+        if (max == 1)
+        {
+            return new ArrayList<>();
+        }
+        else
+        {
+            return modes;
+        }
+    }
+}
